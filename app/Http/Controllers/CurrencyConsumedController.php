837849a4f04454a3252aa84353b951e03e67070e
liudian <hardy.liu@outlook.com>
2017-12-15 11:52:58 +0800

    update: 更新道具消耗记录接口，增加游戏类型和道具的约束条件

--- app/Http/Controllers/CurrencyConsumedController.php
+++ app/Http/Controllers/CurrencyConsumedController.php
@@ -9,15 +9,20 @@ use Illuminate\Http\Request;
 class CurrencyConsumedController extends Controller
 {
     //获取道具消耗记录（此记录包括后台的充值和房卡的消耗）
-    public function getCurrencyLog(ApiRequest $request)
+    public function getCurrencyLog(Request $request)
     {
         $this->validate($request, [
-            'date' => 'date_format:"Y-m-d"'
+            'date' => 'date_format:"Y-m-d"',
+            'item_type' => 'integer', //道具类型
+            'game_kind' => 'integer', //游戏类型
         ]);
 
-        if ($request->has('date')) {
-            return LogCurrencyOperation::whereDate('time', $request->input('date'))->get();
-        }
-        return LogCurrencyOperation::all();
+        return LogCurrencyOperation::when($request->has('date'), function ($query) use ($request) {
+            return $query->whereDate('time', $request->input('date'));
+        })->when($request->has('item_type'), function ($query) use ($request) {
+            return $query->where('type', $request->input('item_type'));
+        })->when($request->has('game_kind'), function ($query) use ($request) {
+            return $query->where('kind', $request->input('game_kind'));
+        })->latest('id')->get();
     }
 }
