dae377efa0bf1d6d49e12a79334fdd40a676dd3a
liudian <hardy.liu@outlook.com>
2018-01-12 17:51:47 +0800

    update: 添加微信公众号取消订阅接口，取消订阅之后移除数据库条目

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -39,6 +39,7 @@ class WechatController extends Controller
                         $this->handleSubscribeEvent($message);
                         break;
                     case 'unsubscribe': //取消关注
+                        $this->handleUnsubscribeEvent($message);
                         break;
                     default:
                         # code...
@@ -57,8 +58,8 @@ class WechatController extends Controller
     protected function handleSubscribeEvent($message)
     {
         $openId = $message->FromUserName;
-        $subcriber = UnionidOpenid::where('openid', $openId)->first();
-        if (!empty($subcriber)) {   //如果不为空说明已经关注了公众号，数据库有保存用户的数据
+        $record = UnionidOpenid::where('openid', $openId)->first();
+        if (!empty($record)) {   //如果不为空说明已经关注了公众号，数据库有保存用户的数据
             return true;
         } else {
             $unionId = $this->getSubscriberUnionId($message->FromUserName);
@@ -76,5 +77,18 @@ class WechatController extends Controller
         $token = $accessToken->getToken();
         $res = WechatService::getUnionId($token, $openId);
         return $res['unionid'];
+    }
+
+    //如果用户取消关注之后，删除数据库对应条目
+    protected function handleUnsubscribeEvent($message)
+    {
+        $openId = $message->FromUserName;
+        $record = UnionidOpenid::where('openid', $openId)->first();
+        if (!empty($record)) {      //如果没有此条记录就忽略之(比如此功能没上线之前就关注了的人数据库是没有条目的)
+            return true;
+        } else {
+            $record->delete();
+            return true;
+        }
     }
 }
